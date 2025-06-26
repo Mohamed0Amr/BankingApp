ed8dcda0bbe4ad83d5bab2210363f58761bfafe3
Mohamed0Amr <140004433+Mohamed0Amr@users.noreply.github.com>
2025-06-26 20:14:56 +0300

    upd

--- NodeJs/Proj-1/db/db.js
+++ NodeJs/Proj-1/db/db.js
@@ -1,11 +1,12 @@
 require('dotenv').config();
 const mysql = require('mysql2');
+require('dotenv').config();
+
 
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER,
-  password: process.env.DB_PASS,
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
 });
 
