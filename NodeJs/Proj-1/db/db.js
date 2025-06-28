c2268b3478902f746a3089fe81adec6c0d72cf75
Mohamed0Amr <140004433+Mohamed0Amr@users.noreply.github.com>
2025-06-28 15:25:56 +0300

    Update

--- NodeJs/Proj-1/db/db.js
+++ NodeJs/Proj-1/db/db.js
@@ -2,24 +2,23 @@ require('dotenv').config();
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  host: process.env.MYSQLHOST || '127.0.0.1',
-  user: process.env.MYSQLUSER || 'root',
-  password: process.env.MYSQLPASSWORD || 'MohamedAmr@2002',
-  database: process.env.MYSQLDATABASE || 'bankingapp',
-  port: process.env.MYSQLPORT || 3306,
+  host: process.env.MYSQLHOST, // Will now get 'mysql.railway.internal'
+  user: process.env.MYSQLUSER, // 'root'
+  password: process.env.MYSQLPASSWORD, // The long random password
+  database: process.env.MYSQLDATABASE, // 'railway'
+  port: process.env.MYSQLPORT, // 3306
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
 
-// Debug logging
-console.log('Railway DB Config:', {
+// Add this debug code
+console.log('Railway DB Connection:', {
   host: process.env.MYSQLHOST,
   user: process.env.MYSQLUSER,
   database: process.env.MYSQLDATABASE,
   port: process.env.MYSQLPORT
 });
-
 // Test connection
 pool.getConnection((err, connection) => {
   if (err) {
